d5acb9f3919b5f9befccae8f9c768622e86c7502
Krimsit <andrejsubbotin2002@yandex.ru>
2019-03-09 22:39:27 +0300

    OAzis

--- js/common.js
+++ js/common.js
@@ -1,12 +1,1 @@
-$(document).ready(function(){
-	$('#menu').click(function() {
-		if($('#menu').hasClass('active')) {
-            $('#nav').removeClass('response');
-            $('#menu').removeClass('active');
-		}
-		else {
-            $('#nav').addClass('response');
-            $('#menu').addClass('active');
-		}
-	});
-})
+
